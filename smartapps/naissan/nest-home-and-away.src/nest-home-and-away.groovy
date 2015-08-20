35e9ad9ecb49f007acf3a3238169e3c01bd259af
Bruce Adelsman <badelsman@visi.com>
2015-08-20 00:58:16 -0500

    Initial version.

---
+++ smartapps/naissan/nest-home-and-away.src/nest-home-and-away.groovy
@@ -0,0 +1,46 @@
+/**
+ *  Nest Home and Away
+ *
+ *  Sets thermostat based on hub mode.
+ */
+
+definition(
+    name:        "Nest Home and Away",
+    namespace:   "naissan",
+    author:      "Bruce Adelsman",
+    description: "Sets the thermostat to away or present based on hub mode.",
+    category:    "Green Living",
+    iconUrl:     "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience.png",
+    iconX2Url:   "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience%402x.png"
+)
+
+preferences {
+
+  section("Change these thermostats modes...") {
+    input "thermostats", "capability.thermostat", multiple: true
+  }
+}
+
+def installed() {
+  state.lastMode = null
+  subscribe(location, changeMode)
+}
+
+def updated() {
+  state.lastMode = null
+  unsubscribe()
+  subscribe(location, changeMode)
+}
+
+def changeMode(evt) {
+
+  if(evt.value == "Away") {
+    log.info("Setting thermostats to away")
+    thermostats?.away()
+  }
+  else if (state.lastMode != null && state.lastMode == "Away") {
+    log.info("Setting thermostats to present")
+    thermostats?.present()
+  }
+  state.lastMode = evt.value
+}
